c82ad6cd2315032887ec2a09adbea26b92b2337e
ProDeveloper-1 <prospermbuma@gmail.com>
2024-06-12 00:59:26 +0300

    Account GUI updates

---
+++ src/main/java/com/celebrate/crdb_bank/Controllers/Client/AccountsController.java
@@ -0,0 +1,20 @@
+package com.celebrate.crdb_bank.Controllers.Client;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AccountsController {
+    public Label ch_acc_num;
+    public Label transaction_limit;
+    public Label ch_acc_date;
+    public Label ch_acc_bal;
+    public Label sv_acc_num;
+    public Label withdrawal_limit;
+    public Label sv_acc_date;
+    public Label sv_acc_bal;
+    public TextField amount_to_sv;
+    public Button trans_to_sv_btn;
+    public TextField amount_to_ch;
+    public Button trans_to_ch_btn;
+}
